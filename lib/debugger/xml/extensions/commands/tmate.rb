ae44cde8e6e810c030c05d15f0f5ecc94caec0bf
Anton Astashov <anton.astashov@gmail.com>
2013-03-14 10:25:42 -0700

    Make sure we don't init tmate command not on darwin platform

--- lib/debugger/xml/extensions/commands/tmate.rb
+++ lib/debugger/xml/extensions/commands/tmate.rb
@@ -1,13 +1,15 @@
 module Debugger
-  class TextMateCommand < Command
+  if RUBY_PLATFORM =~ /darwin/
+    class TextMateCommand < Command
 
-    def execute_with_xml(*args)
-      errmsg(pr("general.errors.unsupported", cmd: 'tmate')) && return if Debugger.printer.type == "xml"
-      execute_without_xml(*args)
+      def execute_with_xml(*args)
+        errmsg(pr("general.errors.unsupported", cmd: 'tmate')) && return if Debugger.printer.type == "xml"
+        execute_without_xml(*args)
+      end
+
+      alias_method :execute_without_xml, :execute
+      alias_method :execute, :execute_with_xml
+
     end
-
-    alias_method :execute_without_xml, :execute
-    alias_method :execute, :execute_with_xml
-
   end
 end
