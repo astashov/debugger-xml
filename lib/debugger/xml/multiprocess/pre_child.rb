86bd963afdc106a0b2f1278bac9596cc2b2d9e16
Oleg Sukhodolsky <os97673@gmail.com>
2013-10-11 21:18:43 +0400

    Initial attempt to provide multi-process debug. Some code copied from ruby-debug-ide.

---
+++ lib/debugger/xml/multiprocess/pre_child.rb
@@ -0,0 +1,75 @@
+module Debugger
+  module Xml
+    module MultiProcess
+      class << self
+        def pre_child
+
+          require 'socket'
+          require 'ostruct'
+
+          host = ENV['DEBUGGER_HOST']
+          port = find_free_port(host)
+
+          options = OpenStruct.new(
+            'frame_bind'  => false,
+            'host'        => host,
+            'load_mode'   => false,
+            'port'        => port,
+            'stop'        => false,
+            'tracing'     => false,
+            'int_handler' => true,
+            wait_for_start: true,
+          )
+
+          acceptor_host, acceptor_port = ENV['IDE_PROCESS_DISPATCHER'].split(":")
+          acceptor_host, acceptor_port = '127.0.0.1', acceptor_host unless acceptor_port
+
+          connected = false
+          3.times do |i|
+            begin
+              s = TCPSocket.open(acceptor_host, acceptor_port)
+              s.print(port)
+              s.close
+              connected = true
+              start_debugger(options)
+              return
+            rescue => bt
+              $stderr.puts "#{Process.pid}: connection failed(#{i+1})"
+              $stderr.puts "Exception: #{bt}"
+              $stderr.puts bt.backtrace.map { |l| "\t#{l}" }.join("\n")
+              sleep 0.3
+            end unless connected
+          end
+        end
+
+        def start_debugger(options)
+          if Debugger.started?
+            # we're in forked child, only need to restart control thread
+            Debugger.breakpoints.clear
+            Debugger.control_thread = nil
+          end
+
+          if options.int_handler
+            # install interruption handler
+            trap('INT') { Debugger.interrupt_last }
+          end
+
+          # set options
+          Debugger.keep_frame_binding = options.frame_bind
+          Debugger.tracing = options.tracing
+          Debugger.wait_for_start = options.wait_for_start
+
+          Debugger.start_remote_ide(options)
+        end
+
+
+        def find_free_port(host)
+          server = TCPServer.open(host, 0)
+          port   = server.addr[1]
+          server.close
+          port
+        end
+      end
+    end
+  end
+end
