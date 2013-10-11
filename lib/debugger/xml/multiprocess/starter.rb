44644804c6c163437465d652353bd13a1438a485
Oleg Sukhodolsky <os97673@gmail.com>
2013-10-11 21:18:43 +0400

    Initial attempt to provide multi-process debug. Some code copied from ruby-debug-ide.

---
+++ lib/debugger/xml/multiprocess/starter.rb
@@ -0,0 +1,8 @@
+if ENV['IDE_PROCESS_DISPATCHER']
+  require 'rubygems'
+  ENV['DEBUGGER_STORED_RUBYLIB'].split(File::PATH_SEPARATOR).each do |path|
+    $LOAD_PATH << path
+  end
+  require 'debugger/xml'
+  Debugger::MultiProcess::pre_child
+end
