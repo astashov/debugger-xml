e4ac2ddfe42341f90ef4805cd9b2334084188337
Anton Astashov <anton.astashov@gmail.com>
2013-10-13 12:17:49 -0700

    Add loggers for Vim and IDE
    Added 3 loggers:
    - Vim Logger, which just appends lines to the given log file
    - IDE logger, which just writes to $stderr
    - Fake logger, which doesn't do anything
    
    We use Fake logger when debug mode is disabled, or for tests.

--- lib/debugger/xml/extensions/vim_server.rb
+++ lib/debugger/xml/extensions/vim_server.rb
@@ -5,29 +5,37 @@ module Debugger
 
     def start_for_vim(options)
       return if @control_thread
+      Xml.logger.puts("Going to daemonize");
       daemonize!
+      Xml.logger.puts("Successfully daemonized");
       $stdout.reopen(options.output_file, 'w')
       $stdout.sync
       $stderr.reopen($stdout)
+      Xml.logger.puts("Redirected stderr");
       @mutex = Mutex.new
       @proceed = ConditionVariable.new
       start
+      Xml.logger.puts("Started debugger");
       File.unlink(options.socket) if File.exist?(options.socket)
       server = UNIXServer.new(options.socket)
       Xml::Vim::Notification.new("establish_connection", options).send
+      Xml.logger.puts("Sent 'established_connection' command");
       @control_thread = DebugThread.new do
         begin
           while (session = server.accept)
+            Xml.logger.puts("Accepted connection");
             interface = Xml::Vim::Interface.new(session, options)
             processor = Xml::Vim::ControlCommandProcessor.new(interface)
             self.handler = Xml::Vim::Processor.new(interface)
+            Xml.logger.puts("Going to process commands");
             processor.process_commands
           end
         rescue Exception => e
-          puts "INTERNAL ERROR!!! #{$!}" rescue nil
-          puts $!.backtrace.map{|l| "\t#{l}"}.join("\n") rescue nil
+          Xml.logger.puts("INTERNAL ERROR!!! #{$!}") rescue nil
+          Xml.logger.puts($!.backtrace.map{|l| "\t#{l}"}.join("\n")) rescue nil
           raise e
         ensure
+          Xml.logger.puts("Closing server");
           server.close
         end
       end
