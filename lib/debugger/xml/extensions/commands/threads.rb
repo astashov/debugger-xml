10b9bfd5000c759495e40640c53e9f105cc1efe0
Oleg Sukhodolsky <os97673@gmail.com>
2013-10-22 11:29:05 +0400

    We need pid in thread's description

---
+++ lib/debugger/xml/extensions/commands/threads.rb
@@ -0,0 +1,10 @@
+module Debugger
+  module ThreadFunctions # :nodoc:
+    def thread_arguments_with_pid(context, should_show_top_frame = true)
+      thread_arguments_without_pid().merge(pid: Process.pid)
+    end
+
+    alias_method :thread_arguments_without_pid, :thread_arguments
+    alias_method :thread_arguments, :thread_arguments_with_pid
+  end
+end
