df7c4de187c58bf3c29c6c870fc22612c2599446
Oleg Sukhodolsky <os97673@gmail.com>
2013-10-23 14:57:21 +0400

    todo

--- lib/debugger/xml/multiprocess/starter.rb
+++ lib/debugger/xml/multiprocess/starter.rb
@@ -1,5 +1,6 @@
 if ENV['IDE_PROCESS_DISPATCHER']
   require 'rubygems'
+  # todo: do we need these lines?
   ENV['DEBUGGER_STORED_RUBYLIB'].split(File::PATH_SEPARATOR).each do |path|
     $LOAD_PATH << path
   end
