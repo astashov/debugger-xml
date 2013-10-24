ed9c0ab3e9ed868284adc6bdcbff00b3f4442f55
Oleg Sukhodolsky <os97673@gmail.com>
2013-10-24 13:45:57 +0400

    Need to pass load paths for debugger and debugger-xml to child process

--- lib/debugger/xml/multiprocess/starter.rb
+++ lib/debugger/xml/multiprocess/starter.rb
@@ -2,8 +2,9 @@ if ENV['IDE_PROCESS_DISPATCHER']
   require 'rubygems'
   # todo: do we need these lines?
   ENV['DEBUGGER_STORED_RUBYLIB'].split(File::PATH_SEPARATOR).each do |path|
+    next unless path =~ /debugger-xml|\/debugger\/|linecache/
     $LOAD_PATH << path
   end
   require 'debugger/xml'
-  Debugger::Xml::MultiProcess::pre_child
+  Debugger::Xml::MultiProcess.pre_child
 end
