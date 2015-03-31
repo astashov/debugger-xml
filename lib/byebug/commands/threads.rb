94278cb9373221caf3d3eabc57c58cb2bae38214
Oleg Sukhodolsky <os97673@gmail.com>
2015-03-31 17:06:27 +0300

    need to provide more thread args

--- lib/byebug/commands/threads.rb
+++ lib/byebug/commands/threads.rb
@@ -1,7 +1,12 @@
 module Byebug
   module ThreadFunctions # :nodoc:
     def thread_arguments_with_pid(context, should_show_top_frame = true)
-      thread_arguments_without_pid(context, should_show_top_frame).merge(pid: Process.pid)
+      thread_arguments_without_pid(context, should_show_top_frame)
+          .merge(
+              pid: Process.pid,
+              status: context.thread.status,
+              current: (context.thread == Thread.current),
+          )
     end
 
     alias_method :thread_arguments_without_pid, :thread_arguments
